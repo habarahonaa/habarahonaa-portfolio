8b7dde603f9b0fedf71225e6c37f7b03b8eef3d1
Hollman A. Barahona <habarahonaa@gmail.com>
2022-08-08 21:45:40 -0600

    fix: React 18 preferred useEffect syntax

--- components/LatestCode.js
+++ components/LatestCode.js
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import getLatestRepos from "@lib/getLatestRepos";
 import userData from "@constants/data";
 
 export default function LatestCode({ repositories }) {
   const [repos, setRepos] = useState([]);
 
-  useEffect(async () => {
-    // let latestRepos = await getLatestRepos(userData);
-    // console.log("latestRepos", latestRepos);
+  useEffect(() => {
     setRepos(repositories);
   }, []);
   return (
@@ -47,7 +44,7 @@ export default function LatestCode({ repositories }) {
 
         {repos &&
           repos.map((latestRepo, idx) => (
-            <GithubRepoCard latestRepo={latestRepo} key="idx" />
+            <GithubRepoCard latestRepo={latestRepo} key={idx} />
           ))}
       </div>
     </section>
